0f4cfd5bbe7ef2d69adf6d6752758c4c49b369f8
Mujtaba225 <106347272+Mujtaba225@users.noreply.github.com>
2022-08-20 16:44:06 +0500

    Add files via upload

---
+++ Strapp.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 20 15:21:35 2022
+
+@author: SRC
+"""
+
+import streamlit as st
+import pandas as pd
+
+
+
+
+def main():
+    st.title('Air Passengers Analysis')
+    data = pd.read_csv(r'C:\Users\SRC\CDA/AirPassengers.csv')
+    month = st.selectbox("Month",list(data['Month'].unique()))
+    st.table(data[data['Month']==month])
+    #st.table(data)
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__=='__main__':
+    main()
